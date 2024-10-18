d1abc40e6c0743603c3c6db57054c97032de4706
Nicolypp <160597860+Nicolypp@users.noreply.github.com>
2024-10-18 18:39:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    'biotecnologia',
+    'o que é biotecnologia?',
+    'É o uso de seres vivos ou partes deles para criar produtos úteis, como remedios ou alimentos'
+)
+
+
+criaCartao(
+    'Geografia',
+    'O que é um bioma? ',
+    'é uma grande area geografica com caracteristicas ambientais e tipos de vegetação semelhantes.'
+
+)
+
+criaCartao(
+    'geografia',
+    'O que é o EL Niño? ',
+    'EL Niño é um fenomeno climatico causado pelo aquecimento anormal das aguas do pacífico, que provoca alteraçoes nos padroes de clima global, como secas e chuvas intensas em diferentes regioes.'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Qual o enredo principal de dom casmurro?',
+    'Dom Casmurro conta a historia de bento santiago, que suspeita que sua esposa Capitu o traiu com seu melhor amigo, Escobar. A dúvida sobre a traição nunca é resolvida, e o romance explora temas como ciúme, ambiguidade e manipulação.'
+)
+
+criaCartao(
+    'Historia',
+    'O que foi a guerra fria?',
+    'Foi um periodo de tensoes politicas e ideologicas entre os Estados Unidos e a União Sovietica, de 1947 e 1991, marcado pela disputa entre o capitalismo e o comunismo, sem confrontos diretos entre as duas superpotencias, mas com guerrars indiretas e uma corrida armamentista'
+)
