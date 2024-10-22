f17b24893bb2ce8cd815ccb0eaa20761ee3344cb
Nicolypp <160597860+Nicolypp@users.noreply.github.com>
2024-10-22 07:42:00 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -29,3 +29,13 @@ criaCartao(
     'O que foi a guerra fria?',
     'Foi um periodo de tensoes politicas e ideologicas entre os Estados Unidos e a União Sovietica, de 1947 e 1991, marcado pela disputa entre o capitalismo e o comunismo, sem confrontos diretos entre as duas superpotencias, mas com guerrars indiretas e uma corrida armamentista'
 )
+criaCartao(
+    'Sociologia',
+    ' o que é antropologia? ',
+    ' uma forma de estudar o ser humano, e de origem grega.'
+)
+criaCartao(
+    'Sociologia',
+    'termo sociologia, quando surgiu como disciplina e para que? ',
+    'O termo sociologia foi criado em 1838 (séc XIX) por Augusto Comte, surgiu como disciplina no século XVIII, como resposta academica para um desafio que estava surgindo: o inicio da sociedade moderna.'
+)
